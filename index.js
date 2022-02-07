cae8475a51b7b56ff877d48cfbf2a547c446c1b7
tylergubser <tylergubser2200@gmail.com>
2022-02-07 15:09:47 -0600

    commit tree 258ddb61d37e48a7a595a6a9c768ed204df61cdc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+main = document.getElementById("main");
+main.remove();
+const newEl = document.createElement("newHeader");
+document.body.append(newEl)
+const element = document.createElement("h1");
+element.setAttribute("h1", "Victory");
+newEl.append(element);
+element.textContent = "YOUR-NAME is the champion";
+
+
+body.append(newEl);
+
+//document.h1.append(newHeader);
